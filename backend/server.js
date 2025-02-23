a79437fca26ce406da741601033b92d506fd06f0
Rahul_Yadav <68394509+rahulyadav232@users.noreply.github.com>
2025-02-24 02:14:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const candidateRoutes = require("./routes/candidateRoutes");
 dotenv.config();
 const app = express();
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: ["https://your-frontend.vercel.app"], // Allow requests from your frontend
+  methods: ["GET", "POST", "PUT", "DELETE], // Allowed methods
+  credentials: true // Allow cookies (if needed)
+}));
 app.use(express.json());
 app.use(cookieParser());
 
